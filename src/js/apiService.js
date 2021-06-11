855af88b32752b3e902efaef3d0271dc58d6dace
Yana Filippova <yanafilippova20@gmail.com>
2021-06-11 17:43:55 +0300

    JS HW-13

---
+++ src/js/apiService.js
@@ -0,0 +1,44 @@
+const API_KEY = '21910767-1ff9d0df6204712ecdebd388c';
+const BASE_URL = 'https://pixabay.com/api';
+
+export default class ImageApiService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
+
+  async fetchImages() {
+    const searchParams = new URLSearchParams({
+      image_type: 'photo',
+      orientation: 'horizontal',
+      q: this.searchQuery,
+      page: this.page,
+      per_page: 12,
+      key: API_KEY,
+    });
+
+    const url = `${BASE_URL}/?${searchParams}`;
+    // const url = `${BASE_URL}/?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${API_KEY}`;
+
+    const response = await fetch(url);
+    const images = await response.json();
+    this.incrementPage();
+    return images.hits;
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+}
---
+++ src/js/modal.js
@@ -0,0 +1,16 @@
+import * as basicLightbox from 'basiclightbox';
+import 'basiclightbox/dist/basicLightbox.min.css';
+
+function onGalleryItemClick(e) {
+  if (e.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  e.preventDefault();
+
+  const instance = basicLightbox.create(`<img src="${e.target.dataset.src}" alt="" />`);
+
+  instance.show();
+}
+
+export { onGalleryItemClick };
